c470ed2a04347bc0ecbddb076aa172c84e3c973e
mygethub-99 <tomakeitcount@gmail.com>
2019-09-21 21:02:03 -0500

    Update dbconnectFunc.py

--- dbconnectFunc.py
+++ dbconnectFunc.py
@@ -7,10 +7,12 @@ SQL code for pulling data from DBs
 import cx_Oracle
 import pyodbc
 import pandas as pd
+
+
         
 def axiomdb():
     #Connects to the Axiom DB
-    conn = cx_Oracle.connect(user ='id', password="klji", dsn="kjkjlj")
+    conn = cx_Oracle.connect(user ='OW4253', password="Breakslowmen6060", dsn="p1c2d831")
     #conn.setencoding(encoding='utf-8')
     q = ("select distinct start_date, eutran_cell_fdd_id, freqband, earfcndl,\
          dl_channel_bandwidth, administrative_state\
@@ -23,16 +25,17 @@ def axiomdb():
 def iqiDB(l1,mon):
     #q=[]
     #Connects to the IQI DB
-
-    conn3 = pyodbc.connect(DSN="aDB", UID="id", PWD= "*#@%@")
+#l1 =["-87.569122","33.162369","-87.509122","33.222369"]
+#mon = ['201907','201908']
+    conn3 = pyodbc.connect(DSN="SRVVERTICA", UID="OW4253", PWD= "Breakfoodall6060")
     
     q1= "select yyyymm, most_used_ecgi_cellname mostCell,\
         most_used_ecgi_freq freqMostUd, avg_rsrp, avg_rsrq, rsrq_recs,\
         handsets_lte, rrc_attempted numrcc, rrc_failed rccFail,\
         rrc_reestab_attempted, rrc_reestab_failed, noservice_events NSNum,\
-        noservice_duration NSDura, sw_long lat, sw_lat long, market "
+        noservice_duration NSDura, sw_long long, sw_lat lat, market "
     q1 += "FROM IQI_SUMMARY.iqi_mgrs10m_monthly_summary WHERE yyyymm in\
-        ('201906','201907')"
+        ('201908')"
     q1 += "and sw_long >= (%s) and sw_long <= (%s)\
         and sw_lat >= (%s) and sw_lat <= (%s)" % (l1[0],l1[2],l1[1],l1[3])
     q1 += "GROUP BY yyyymm, most_used_ecgi_cellname,avg_rsrp, avg_rsrq,\
