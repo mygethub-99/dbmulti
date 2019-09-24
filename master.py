ba26dc9ba22db8b8a1b8b29afcb20f7428b8ee54
mygethub-99 <tomakeitcount@gmail.com>
2019-09-23 22:58:28 -0500

    Update master.py

--- master.py
+++ master.py
@@ -13,28 +13,34 @@ from shapely.geometry import Point
 
 from geopoints import buffer, geosites, bounds, geoiqi
 from dbconnectFunc import iqiDB
+from readshp import highway
 #Create map of existing sites, buffers, bounds, iqi
 dates=['201907','201908']
 sites=geosites()
 buf=buffer()
 bound=bounds()
-usid_list=[]
+highway=highway()
+
 #Execute iqi query on each new site based on bounds, name it by usid
 #Create GeoDataFrame with lat and long geometry for plotting
 for i, row in bound.iterrows():
     bd=row['minx'],row['miny'],row['maxx'],row['maxy']
     site='_'+str(i)
     df=iqiDB(bd,dates)
-    usid_list.append(i)
     exec("{}=geoiqi(df)" .format(site))
 #Create plots
-for i in usid_list:
+for i, row in bound.iterrows():
     site = '_'+str(i)
+    #fig= plt.figure()
     plt.rcParams['figure.figsize'] = (4,3)
     plt.rcParams['figure.dpi'] = 150
-    exec("ax={}[{}['avg_rsrp'] > -105].plot(markersize = 2, color = 'blue', marker = 'o', alpha=.05)" .format(site,site))
+    ax=sites.plot(color='red',markersize=5)
+    exec("ax={}[{}['avg_rsrp'] > -105].plot(ax=ax,markersize = 2, color = 'blue', marker = 'o', alpha=.05)" .format(site,site))
     sites.plot(ax=ax, color='red',markersize=5)
+    highway.plot(ax=ax, color = 'black', alpha=.5)
     black_label = mpatches.Patch(color='black', label='-105RSRP 10Meter IQI')
     plt.legend(handles=[black_label])
+    ax.set_xlim([row['minx'],row['maxx']])
+    ax.set_ylim([row['miny'],row['maxy']])
     plt.show()
     plt.savefig(r'C:\Users\ow4253\Documents\development\vertica\dbscripts\plots\{}.pdf'.format(site))
