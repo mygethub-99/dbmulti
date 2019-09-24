4c1abcdb5b071e866f2937b03bfdeeccc4406f82
mygethub-99 <tomakeitcount@gmail.com>
2019-09-23 23:08:03 -0500

    Update dbconnectFunc.py

--- dbconnectFunc.py
+++ dbconnectFunc.py
@@ -9,9 +9,8 @@ import pyodbc
 import pandas as pd
 
 
-        
+#Connect and query axiom db        
 def axiomdb():
-    #Connects to the Axiom DB
     conn = cx_Oracle.connect(user ='OW4253', password="Breakslowmen6060", dsn="p1c2d831")
     #conn.setencoding(encoding='utf-8')
     q = ("select distinct start_date, eutran_cell_fdd_id, freqband, earfcndl,\
@@ -22,13 +21,9 @@ def axiomdb():
     return axiom
     conn.close()
 
+#Connect and query iqi db
 def iqiDB(l1,mon):
-    #q=[]
-    #Connects to the IQI DB
-#l1 =["-87.569122","33.162369","-87.509122","33.222369"]
-#mon = ['201907','201908']
     conn3 = pyodbc.connect(DSN="SRVVERTICA", UID="OW4253", PWD= "Breakfoodall6060")
-    
     q1= "select yyyymm, most_used_ecgi_cellname mostCell,\
         most_used_ecgi_freq freqMostUd, avg_rsrp, avg_rsrq, rsrq_recs,\
         handsets_lte, rrc_attempted numrcc, rrc_failed rccFail,\
@@ -42,8 +37,6 @@ def iqiDB(l1,mon):
         rsrq_recs, handsets_lte, rrc_attempted,rrc_failed, rrc_reestab_attempted, \
         rrc_reestab_failed, noservice_events, most_used_ecgi_freq,\
         noservice_duration, market, sw_lat, sw_long ORDER BY mostCell"
-    
-    
     iqi = pd.read_sql(q1, conn3)
     return iqi
     conn3.close()
