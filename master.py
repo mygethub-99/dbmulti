bd8fd41b9557c4cecec484e08a1b3df1f91b3b9c
mygethub-99 <tomakeitcount@gmail.com>
2019-09-24 10:59:42 -0500

    Update master.py

--- master.py
+++ master.py
@@ -10,6 +10,9 @@ import matplotlib.patches as mpatches
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import Point
+SMALL_SIZE = 3
+MEDIUM_SIZE = 6
+BIGGER_SIZE = 8
 
 from geopoints import buffer, geosites, bounds, geoiqi
 from dbconnectFunc import iqiDB
@@ -33,13 +36,21 @@ for i, row in bound.iterrows():
     site = '_'+str(i)
     #fig= plt.figure()
     plt.rcParams['figure.figsize'] = (4,3)
-    plt.rcParams['figure.dpi'] = 150
+    plt.rcParams['figure.dpi'] =250
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
     ax=sites.plot(color='red',markersize=5)
     exec("ax={}[{}['avg_rsrp'] > -105].plot(ax=ax,markersize = 2, color = 'blue', marker = 'o', alpha=.05)" .format(site,site))
     sites.plot(ax=ax, color='red',markersize=5)
     highway.plot(ax=ax, color = 'black', alpha=.5)
     black_label = mpatches.Patch(color='black', label='-105RSRP 10Meter IQI')
-    plt.legend(handles=[black_label])
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),handles=[black_label])
+    plt.title('USID'+site)
     ax.set_xlim([row['minx'],row['maxx']])
     ax.set_ylim([row['miny'],row['maxy']])
     plt.show()
